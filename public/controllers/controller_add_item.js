123561d7db961ebf6ab7fc6f1c7d67cb45c51533
Basel <BaselSafieh@gmail.com>
2017-02-17 12:23:45 +0100

    Initial Bootstrab integration.

--- public/controllers/controller_add_item.js
+++ public/controllers/controller_add_item.js
@@ -4,54 +4,55 @@
 
 angular.module('myApp.add_item', ['ngRoute'])
 
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/addItem', {
             templateUrl: 'views/add_item.html',
             controller: 'controller_add_item'
         });
     }])
 
-    .controller('controller_add_item', ['$scope', '$http', function($scope, $http) {
+    .controller('controller_add_item', ['$scope', '$http', function ($scope, $http) {
 
-        $scope.prop = {"type": "select",
+        $scope.prop = {
+            "type": "select",
             "name": "Service",
             "value": "Books",
             "values": ['Books', 'Tools', 'Games', 'Others']
         };
 
-        $scope.submit = function(){
+        $scope.submit = function () {
             /*
-            To use the temporary server POST request, we need 2 things:
-            1- Add the content-type: application/json header to the request
-            2- Send in the body, the data as JSON object
-            Also, the JSON file has to contain an array called items that has objects with name, desc, category keys
-            */
+             To use the temporary server POST request, we need 2 things:
+             1- Add the content-type: application/json header to the request
+             2- Send in the body, the data as JSON object
+             Also, the JSON file has to contain an array called items that has objects with name, desc, category keys
+             */
             var date1 = $scope.item.expDate;
             var date2 = new Date();
             var date3 = date1.getFullYear() + '-' + date1.getMonth() + '-' + date1.getDate();
             var date4 = date2.getFullYear() + '-' + date2.getMonth() + '-' + date2.getDate();
 
             $http({
-                method : "POST",
-                url : "http://198.211.126.133:3000/additem",
+                method: "POST",
+                url: "http://198.211.126.133:3000/additem",
                 headers: {
                     'Content-Type': "application/json"
                 },
                 data: {
-                    "item_name" : $scope.item.name,
-                    "description" : $scope.item.desc,
-                    "category" : $scope.prop.value,
-                    "expiration_date" : date3,
-                    "submission_date" : date4,
-                    "location" : "8374839,8437839"
+                    "item_name": $scope.item.name,
+                    "description": $scope.item.desc,
+                    "category": $scope.prop.value,
+                    "expiration_date": date3,
+                    "submission_date": date4,
+                    "location": "8374839,8437839"
                 }
             }).then(function mySucces(response) {
-			if(response.data.code == 101){
-             			   alert("Success, response is: " + response.data.message);
-			}
-			else{
-				  alert("ERROR: " + response.data.code + "MESSAGE: " + response.data.message);
-			}
+                if (response.data.code == 101) {
+                    alert("Success, response is: " + response.data.message);
+                }
+                else {
+                    alert("ERROR: " + response.data.code + "MESSAGE: " + response.data.message);
+                }
             }, function myError(response) {
                 alert("Error, response is: " + response.data);
             });
--- public/controllers/controller_add_user.js
+++ public/controllers/controller_add_user.js
@@ -4,37 +4,42 @@
 
 angular.module('myApp.add_user', ['ngRoute'])
 
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/registerUser', {
             templateUrl: 'views/add_user.html',
             controller: 'controller_add_user'
         });
     }])
 
-    .controller('controller_add_user', ['$scope', '$http', function($scope, $http) {
+    .controller('controller_add_user', ['$scope', '$http', function ($scope, $http) {
 
-        $scope.submit = function(){
+        $scope.submit = function () {
 
             var date1 = $scope.user.dob;
             var date3 = date1.getFullYear() + '-' + date1.getMonth() + '-' + date1.getDate();
 
             $http({
-                method : "POST",
-                url : "http://198.211.126.133:3000/register",
+                method: "POST",
+                url: "http://198.211.126.133:3000/register",
                 headers: {
                     'Content-Type': "application/json"
                 },
                 data: {
-                    "last_name" : $scope.user.lname,
-                    "first_name" : $scope.user.fname,
-                    "address" : "000,000",
-                    "date_of_birth" : date3,
-                    "email" : $scope.user.email,
-                    "phone" : $scope.user.phone,
-                    "password" : $scope.user.password
+                    "last_name": $scope.user.lname,
+                    "first_name": $scope.user.fname,
+                    "address": "000,000",
+                    "date_of_birth": date3,
+                    "email": $scope.user.email,
+                    "phone": $scope.user.phone,
+                    "password": $scope.user.password
                 }
             }).then(function mySucces(response) {
-                alert(response);
+                if (response.data.code == 101) {
+                    alert("Success, response is: " + response.data.message);
+                }
+                else {
+                    alert("ERROR: " + response.data.code + "MESSAGE: " + response.data.message);
+                }
             }, function myError(response) {
                 alert("Error, response is: " + response);
             });
