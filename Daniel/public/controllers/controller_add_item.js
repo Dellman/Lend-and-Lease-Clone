fd60e134288571d78f07756702b288e33c695959
Sakvad <dellman307307@gmail.com>
2017-02-17 19:03:39 +0100

    Added click functions

---
+++ Daniel/public/controllers/controller_add_item.js
@@ -0,0 +1,58 @@
+/**
+ * Created by Basel on 2/15/2017.
+ */
+
+angular.module('myApp.add_item', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/addItem', {
+            templateUrl: 'views/add_item.html',
+            controller: 'controller_add_item'
+        });
+    }])
+
+    .controller('controller_add_item', ['$scope', '$http', function($scope, $http) {
+
+        $scope.prop = {"type": "select",
+            "name": "Service",
+            "value": "Books",
+            "values": ['Books', 'Tools', 'Games', 'Others']
+        };
+
+        $scope.submit = function(){
+            /*
+            To use the temporary server POST request, we need 2 things:
+            1- Add the content-type: application/json header to the request
+            2- Send in the body, the data as JSON object
+            Also, the JSON file has to contain an array called items that has objects with name, desc, category keys
+            */
+            var date1 = $scope.item.expDate;
+            var date2 = new Date();
+            var date3 = date1.getFullYear() + '-' + date1.getMonth() + '-' + date1.getDate();
+            var date4 = date2.getFullYear() + '-' + date2.getMonth() + '-' + date2.getDate();
+
+            $http({
+                method : "POST",
+                url : "http://198.211.126.133:3000/additem",
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                data: {
+                    "item_name" : $scope.item.name,
+                    "description" : $scope.item.desc,
+                    "category" : $scope.prop.value,
+                    "expiration_date" : date3,
+                    "submission_date" : date4,
+                    "location" : "8374839,8437839"
+                }
+            }).then(function mySucces(response) {
+                alert("Sucess, response is: " + response);
+            }, function myError(response) {
+                alert("Error, response is: " + response);
+            });
+
+        };
+
+    }]);
+
+
