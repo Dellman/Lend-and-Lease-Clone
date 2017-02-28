85b878a71537741630fda43b7f10cc54a5d9a7b4
Davidovski <davidovski@live.se>
2017-02-28 15:19:17 +0100

    server overhaul

---
+++ app/routes.js
@@ -0,0 +1,165 @@
+module.exports = function(app, passport) {
+
+
+  app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+
+
+
+  app.get('/items', function(req,res){
+      console.log("items_page accessed");
+      connection.query('SELECT * FROM items', function(error, result, fields){
+          res.send(result);
+          console.log("connected");
+      })
+  })
+
+
+  app.get('/profile', function(req,res){
+      console.log("personal profile accessed");
+      var sql = mysql.format("SELECT * FROM users WHERE email = ?", [logged_in_user]);
+      connection.query(sql, function(err, rows, fields) {
+          if(!err && rows.length == 1){
+              res.send(rows[0]);
+              console.log("success with query in view profile");
+  	    console.log(logged_in_user);
+          }else if(sql == null){
+  		res.send("wrong LOGGED IN USER" + logged_in_user);
+  		console.log(logged_in_user);
+          }
+          else{
+              console.log(logged_in_user);
+          }
+
+      });
+
+
+  })
+
+  app.post('/login', function(req, res){
+      console.log("login_page accessed");
+      var post = {
+          email: req.body.email,
+          password: req.body.password
+      };
+      console.log(post);
+      var sql = mysql.format("SELECT * FROM (SELECT * FROM users WHERE email = ?) AS filter WHERE password = ?", [post.email, post.password]);
+      console.log(sql);
+      connection.query(sql, function(err, rows, fields) {
+          if(!err && rows.length == 1){
+              res.send(new response_status(101, "success at login"));
+              console.log("success with query in login");
+  	    logged_in_user = post.email;
+  	    console.log(logged_in_user);
+          }else if(rows.length == 0){
+              res.send(new response_status(601, "Wrong username or password"));
+              console.log(err);
+          }
+          else{
+  		console.log("MYSQL ERROR: " + err);
+  		console.log(rows.length);
+              	console.log("Something wrong at login" + post.email + "PASSWORD: " + post.password);
+          }
+
+      });
+
+  });
+
+  app.post('/additem', function(req, res){
+      console.log("additem_page accessed");
+      var post = {
+          item_name: req.body.item_name,
+          description: req.body.description,
+          category: req.body.category,
+          submission_date: req.body.submission_date,
+          expiration_date: req.body.expiration_date,
+          location: req.body.location
+      };
+
+
+      var sql = mysql.format("SELECT * FROM items WHERE item_name = ? AND location = ?", [post.item_name, post.location]);
+  	console.log(sql);
+      connection.query(sql, function(err, rows, fields) {
+          if(rows == null || rows.length == 0){
+              console.log(post);
+              connection.query('INSERT INTO items SET ?', post, function(err, result){
+                  if(!err){
+                      res.send(new response_status(102, "success at add item"));
+                      console.log("success");
+                  }
+                  else{
+                      res.send(new response_status(801, "Failed to add new item"));
+                      console.log(err);
+                  }
+              });
+          }
+          else{
+              	console.log("ITEM ALREADY EXISTS: " + post.item_name);
+  		res.send(new response_status(802, "Item already added to users items"));
+
+          }
+
+      });
+  });
+
+
+  app.post('/register', function(req, res){
+      console.log("register_page accessed");
+      var post = {
+          last_name: req.body.last_name,
+          first_name: req.body.first_name,
+          address: req.body.address,
+          date_of_birth: req.body.date_of_birth,
+          email: req.body.email,
+          phone: req.body.phone,
+          password: req.body.password
+      };
+      console.log(post);
+      var sql = mysql.format("SELECT * FROM users WHERE email=?", [post.email]);
+
+      connection.query(sql, function(err, rows, fields) {
+          console.log("ROWS LENGTH IS: " + rows.length);
+
+          if(rows.length == 0){
+              connection.query('INSERT INTO users SET ?', post, function(err, result){
+                  if(!err){
+                      res.send(new response_status(103, "success"));
+                      console.log("user: " + post.email + " successfully registered");
+                  }
+                  else{
+                      res.send(new response_status(701, "Failed to register user"));
+                      console.log(err);
+                  }
+              });
+          }
+          else{
+              res.send(new response_status(702, "User with that email already exists"));
+              console.log("EMAIL ALREADY IN USE !!! " + post.email);
+          }
+      });
+
+  });
+  /* *********************** END OF SQL SECTION ***********************
+   ******************************************************************** */
+
+  /* *********************** WEB APP SECTION ***************************
+   ********************************************************************* */
+  app.get('/', function(req, res){
+
+      res.sendFile(public/index.html);
+
+  });
+
+
+      // process the signup form
+      app.post('/signup', passport.authenticate('local-signup', {
+          successRedirect : '/profile', // redirect to the secure profile section
+          failureRedirect : '/signup', // redirect back to the signup page if there is an error
+          failureFlash : true // allow flash messages
+      }));
+
+
+
+}
--- server.js
+++ server.js
@@ -3,6 +3,22 @@ var mysql = require('mysql');
 var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
+var passport = require('passport');
+var flash = require('connect-flash');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.set('view engine', 'ejs');
+app.use(session({ secret: 'adebisi'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -14,6 +30,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 var logged_in_user;
+
+
+function isLoggedIn(req, res, next){
+
+      if (req.isAuthenticated())
+        return next();
+
+      res.redirect('/');
+
+
+}
+
+
+
 
 var response_status = function(code, message){
 	this.code = code;
@@ -31,149 +61,6 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-app.get('/items', function(req,res){
-    console.log("items_page accessed");
-    connection.query('SELECT * FROM items', function(error, result, fields){
-        res.send(result);
-        console.log("connected");
-    })
-})
-
-
-app.get('/profile', function(req,res){
-    console.log("personal profile accessed");
-    var sql = mysql.format("SELECT * FROM users WHERE email = ?", [logged_in_user]);
-    connection.query(sql, function(err, rows, fields) {
-        if(!err && rows.length == 1){
-            res.send(rows[0]);
-            console.log("success with query in view profile");
-	    console.log(logged_in_user);
-        }else if(sql == null){
-		res.send("wrong LOGGED IN USER" + logged_in_user);
-		console.log(logged_in_user);
-        }
-        else{
-            console.log(logged_in_user);
-        }
-
-    });
-
-
-})
-
-app.post('/login', function(req, res){
-    console.log("login_page accessed");
-    var post = {
-        email: req.body.email,
-        password: req.body.password
-    };
-    console.log(post);
-    var sql = mysql.format("SELECT * FROM (SELECT * FROM users WHERE email = ?) AS filter WHERE password = ?", [post.email, post.password]);
-    console.log(sql);
-    connection.query(sql, function(err, rows, fields) {
-        if(!err && rows.length == 1){
-            res.send(new response_status(101, "success at login"));
-            console.log("success with query in login");
-	    logged_in_user = post.email;
-	    console.log(logged_in_user);
-        }else if(rows.length == 0){
-            res.send(new response_status(601, "Wrong username or password"));
-            console.log(err);
-        }
-        else{
-		console.log("MYSQL ERROR: " + err);
-		console.log(rows.length);	
-            	console.log("Something wrong at login" + post.email + "PASSWORD: " + post.password);
-        }
-
-    });
-
-});
-
-app.post('/additem', function(req, res){
-    console.log("additem_page accessed");
-    var post = {
-        item_name: req.body.item_name,
-        description: req.body.description,
-        category: req.body.category,
-        submission_date: req.body.submission_date,
-        expiration_date: req.body.expiration_date,
-        location: req.body.location
-    };
-
-
-    var sql = mysql.format("SELECT * FROM items WHERE item_name = ? AND location = ?", [post.item_name, post.location]);
-	console.log(sql);
-    connection.query(sql, function(err, rows, fields) {
-        if(rows == null || rows.length == 0){
-            console.log(post);
-            connection.query('INSERT INTO items SET ?', post, function(err, result){
-                if(!err){
-                    res.send(new response_status(102, "success at add item"));
-                    console.log("success");
-                }
-                else{
-                    res.send(new response_status(801, "Failed to add new item"));
-                    console.log(err);
-                }
-            });
-        }
-        else{
-            	console.log("ITEM ALREADY EXISTS: " + post.item_name);
-		res.send(new response_status(802, "Item already added to users items"));	
-
-        }
-
-    });
-});
-
-
-app.post('/register', function(req, res){
-    console.log("register_page accessed");
-    var post = {
-        last_name: req.body.last_name,
-        first_name: req.body.first_name,
-        address: req.body.address,
-        date_of_birth: req.body.date_of_birth,
-        email: req.body.email,
-        phone: req.body.phone,
-        password: req.body.password
-    };
-    console.log(post);
-    var sql = mysql.format("SELECT * FROM users WHERE email=?", [post.email]);
-
-    connection.query(sql, function(err, rows, fields) {
-        console.log("ROWS LENGTH IS: " + rows.length);
-
-        if(rows.length == 0){
-            connection.query('INSERT INTO users SET ?', post, function(err, result){
-                if(!err){
-                    res.send(new response_status(103, "success"));
-                    console.log("user: " + post.email + " successfully registered");
-                }
-                else{
-                    res.send(new response_status(701, "Failed to register user"));
-                    console.log(err);
-                }
-            });
-        }
-        else{
-            res.send(new response_status(702, "User with that email already exists"));
-            console.log("EMAIL ALREADY IN USE !!! " + post.email);
-        }
-    });
-
-});
-/* *********************** END OF SQL SECTION ***********************
- ******************************************************************** */
-
-/* *********************** WEB APP SECTION ***************************
- ********************************************************************* */
-app.get('/', function(req, res){
-
-    res.sendFile(public/index.html);
-
-});
 
 /* ******************END OF WEB APP SECTION ***************************
  ********************************************************************* */
