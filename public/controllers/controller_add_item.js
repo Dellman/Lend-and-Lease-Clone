1b00761811146d1e0fc420c385e430999e418526
Basel <BaselSafieh@gmail.com>
2017-03-02 10:58:34 +0100

    Solving Merge issues

--- public/controllers/controller_add_item.js
+++ public/controllers/controller_add_item.js
@@ -1,7 +1,6 @@
 /**
  * Created by Basel on 2/15/2017.
  */
-
 angular.module('myApp.add_item', ['ngRoute'])
 
     .config(['$routeProvider', function ($routeProvider) {
@@ -11,21 +10,98 @@ angular.module('myApp.add_item', ['ngRoute'])
         });
     }])
 
-    .controller('controller_add_item', ['$scope', '$http', 'NgMap', function ($scope, $http, NgMap) {
+    .controller('controller_add_item', ['$scope', '$http', 'NgMap', '$rootScope', function ($scope, $http, NgMap, $rootScope) {
+
+        $("#search_bar").hide();
+
+        //Date Picker
+        var start = moment();//.subtract(29, 'days');
+        var end = moment().add(29, 'days');
+
+        function cb(start, end) {
+            $('#reportrange_right span').html(start.format('DD MMMM, YYYY') + ' - ' + end.format('DD MMMM, YYYY'));
+        }
+
+        $('#reportrange_right').daterangepicker({
+            startDate: start,
+            endDate: end,
+            ranges: {
+                //'Today': [moment(), moment()],
+                //'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Next 7 Days': [moment(), moment().add(6, 'days')],
+                'Next 30 Days': [moment(), moment().add(29, 'days')],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Next Month': [moment().add(1, 'month').startOf('month'), moment().add(1, 'month').endOf('month')]
+            }
+        }, cb);
+        cb(start, end);
+        //------------------------------------
+        //Other Categories
+        $scope.mainCategories = {
+            "type": "select",
+            "name": "Service",
+            "value": "Choose One",
+            "values": [ "Choose One", "Books", "Tools", "Games", "Electronics"]
+        };
+        $scope.subCategories = {
+            "type": "select",
+            "name": "Service",
+            "value": "Choose One",
+            "values": {
+                'Books': ['Choose One', 'Fiction', 'Horror', 'Romance'],
+                'Games': ['Choose One', 'Action', 'Shooters', 'Hack and Slash', 'RPG', 'Strategy', 'Simulation', 'Sport', 'Racing', 'Adventure'],
+                'Tools': ['Choose One', 'Useful', 'Useless'],
+                'Electronics': ['Choose One', 'Indoor', 'Outdoor']
+            }
+        };
+        $scope.categoriesViews = {
+            'Books': false,
+            'Tools': false,
+            'Games': false,
+            'Electronics': false
+        };
+        function resetCategories(){
+            for (var prop in $scope.categoriesViews) {
+                if ($scope.categoriesViews.hasOwnProperty(prop)) {
+                    $scope.categoriesViews[prop] = false;
+                }
+            }
+        }
+        $scope.valueChanged = function(cat){
+            switch (cat){
+                case 'Books':
+                    resetCategories();
+                    $scope.categoriesViews.Books = true;
+                    break;
+                case 'Tools':
+                    resetCategories();
+                    $scope.categoriesViews.Tools = true;
+                    break;
+                case 'Games':
+                    resetCategories();
+                    $scope.categoriesViews.Games = true;
+                    break;
+                case 'Electronics':
+                    resetCategories();
+                    $scope.categoriesViews.Electronics = true;
+                    break;
+            }
+        }
+        //----------------------------------
+
 
         $scope.vm = {};
         NgMap.getMap().then(function(map) {
             $scope.vm.map = map;
             //setupListener($scope.map, 'click');
-
         });
 
-        $scope.prop = {
+/*        $scope.prop = {
             "type": "select",
             "name": "Service",
             "value": "Books",
             "values": ['Books', 'Tools', 'Games', 'Others']
-        };
+        };*/
 
 /*
         function setupListener(map, name) {
@@ -49,6 +125,7 @@ angular.module('myApp.add_item', ['ngRoute'])
           map: $scope.map
         });
       // Remove old marker(s) and add one and then zoom in
+        console.log(itemMarkers.length);
         removeMarkers($scope.map);
         itemMarkers.push(marker);
         putOnMap($scope.map);
@@ -58,6 +135,7 @@ angular.module('myApp.add_item', ['ngRoute'])
 
     function putOnMap(map){
       for (var i = 0; i < itemMarkers.length; i++) {
+        console.log(itemMarkers.length);
         itemMarkers[i].setAnimation(google.maps.Animation.DROP);
         itemMarkers[i].setMap(map);
       }
@@ -72,50 +150,44 @@ angular.module('myApp.add_item', ['ngRoute'])
       itemMarkers.length = 0;
     }
 
-    // Convert cords to readable format
-    function geocodeCords(positionObject){
-      var geocoder = new google.maps.Geocoder;
-      posLat = positionObject.lat;
-      posLng = positionObject.lng;
-      cordsPos = posLat + ", " + posLng;
-      console.log(cordsPos);
-
-      var latlngStr = cordsPos.split(',', 2);
-      var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
-      geocoder.geocode({'location': latlng}, function(results, status) {
-        if (status === 'OK') {
-          if (results[1]) {
-            namePos = (results[1].formatted_address);
-            locationInput.value = namePos;
-            addMarker(positionObject);
-          } else {
-            window.alert('No results found');
-          }
-        } else {
-          window.alert('Geocoder failed due to: ' + status);
-        }
-      });
-    }
 
       // Get User Location
       $scope.getCurLoc = function() {
         // Try HTML5 geolocation.
         // Google geocoder
-        // var geocoder = new google.maps.Geocoder;
+        var geocoder = new google.maps.Geocoder;
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
             var pos = {
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-            geocodeCords(pos);
-          });
-            }
+                posLat = pos.lat;
+                posLng = pos.lng
+                cordsPos = posLat + ", " + posLng;
+
+                var latlngStr = cordsPos.split(',', 2);
+                var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+                geocoder.geocode({'location': latlng}, function(results, status) {
+                  if (status === 'OK') {
+                    if (results[1]) {
+                      namePos = (results[1].formatted_address);
+                      locationInput.value = namePos;
+                      addMarker(pos);
+                    } else {
+                      window.alert('No results found');
+                    }
+                  } else {
+                    window.alert('Geocoder failed due to: ' + status);
+                  }
+                });
+            });
           }
+      }
 
       $scope.checkLoc = function(){
         // Google geocoder
-        // var geocoder = new google.maps.Geocoder;
+        var geocoder = new google.maps.Geocoder;
        geocoder.geocode({'address': locationInput.value}, function(results, status) {
          if (status === 'OK') {
            $scope.map.setCenter(results[0].geometry.location);
@@ -137,29 +209,38 @@ angular.module('myApp.add_item', ['ngRoute'])
         //{lat: 59.6, lng: 17.4}//"(" + locN + ", " + locE + ")";
     }
 
+
         $scope.submit = function () {
+/*            alert("Name: " + $scope.item.name +
+                ", desc: " + $scope.item.desc +
+                ", cat: " + $scope.mainCategories.value +
+                ", subCat: " + $scope.subCategories.value +
+                ", start date: " + $("#reportrange_right").data('daterangepicker').startDate.format('YYYY-MM-DD') +
+                ", end date: " + $("#reportrange_right").data('daterangepicker').endDate.format('YYYY-MM-DD') +
+                ", submission date: " + new Date());*/
             /*
              To use the temporary server POST request, we need 2 things:
              1- Add the content-type: application/json header to the request
              2- Send in the body, the data as JSON object
              Also, the JSON file has to contain an array called items that has objects with name, desc, category keys
              */
-            var date1 = $scope.item.expDate;
+            // var date1 = $scope.item.expDate;
             var date2 = new Date();
-            var date3 = date1.getFullYear() + '-' + date1.getMonth() + '-' + date1.getDate();
+            // var date3 = date1.getFullYear() + '-' + date1.getMonth() + '-' + date1.getDate();
             var date4 = date2.getFullYear() + '-' + date2.getMonth() + '-' + date2.getDate();
 
             $http({
                 method: "POST",
-                url: "http://198.211.126.133:3000/additem",
+                url: $rootScope.serverIP + "/additem",
                 headers: {
                     'Content-Type': "application/json"
                 },
                 data: {
                     "item_name": $scope.item.name,
                     "description": $scope.item.desc,
-                    "category": $scope.prop.value,
-                    "expiration_date": date3,
+                    "category": $scope.mainCategories.value,
+                    "start_date": $("#reportrange_right").data('daterangepicker').startDate.format('YYYY-MM-DD'),
+                    "end_date": $("#reportrange_right").data('daterangepicker').endDate.format('YYYY-MM-DD'),
                     "submission_date": date4,
                     // "location": generateLocation()
                     "location": cordsPos
@@ -177,4 +258,5 @@ angular.module('myApp.add_item', ['ngRoute'])
             });
 
         };
+
     }]);
