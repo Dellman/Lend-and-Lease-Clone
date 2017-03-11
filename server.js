81cf2239366542affef1d5590084782eeb1cdcf6
vuxnamannen <edvin.wahlberg@gmail.com>
2017-03-11 11:40:35 +0100

    server can be run on localhost -- enjoy!

--- server.js
+++ server.js
@@ -28,13 +28,14 @@ var connection = mysql.createConnection({
 connection.connect();
 
 
-/*
+
+
  app.use(function(req, res, next) {
  res.header("Access-Control-Allow-Origin", "*");
  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
  next();
  });
- */
+ 
 /*
  app.use(function(req, res, next) {
  res.setHeader("Access-Control-Allow-Origin", "*");
