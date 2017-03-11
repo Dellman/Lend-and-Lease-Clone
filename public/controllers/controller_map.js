b647284b3c443a782439ba8c2e0cd0e9bd642adc
Basel <BaselSafieh@gmail.com>
2017-03-11 16:38:24 +0100

    Small changes to the Map page

--- public/controllers/controller_map.js
+++ public/controllers/controller_map.js
@@ -11,53 +11,67 @@ angular.module('myApp.map', ['ngRoute', 'ngMap'])
         });
     }])
 
-    .controller('controller_map', ['$scope', '$http', 'NgMap', function ($scope, $http, NgMap) {
+    .controller('controller_map', ['$scope', '$http', 'NgMap', '$rootScope', function ($scope, $http, NgMap, $rootScope) {
 
-        NgMap.getMap({id: 'mapViewMap'}).then(function(map) {
-         $scope.map = map;
+        $http({
+            method: "GET",
+            url: $rootScope.serverIP + "/items",
+            headers: {
+                'Content-Type': "application/json"
+            }
+        }).then(function mySucces(response) {
+            $scope.items = response.data;
+            console.log($scope.items);
+        }, function myError(response) {
+            console.log("Error");
+        });
+
+
+        NgMap.getMap({id: 'mapViewMap'}).then(function (map) {
+            $scope.map = map;
         });
 
         // Convert cords to readable format
-        function geocodeCords(positionObject){
-          var geocoder = new google.maps.Geocoder;
-          var namePos;
-          var posLat = positionObject.position.lat();
-          var posLng = positionObject.position.lng();
+        function geocodeCords(positionObject) {
+            var geocoder = new google.maps.Geocoder;
+            var namePos;
+            var posLat = positionObject.position.lat();
+            var posLng = positionObject.position.lng();
 
-          var latlng = {lat: parseFloat(posLat), lng: parseFloat(posLng)};
-          geocoder.geocode({'location': latlng}, function(results, status) {
-            if (status === 'OK') {
-              if (results[1]) {
-                namePos = (results[1].formatted_address);
-                // console.log(this);
-                this.address = namePos;
-              } else {
-                window.alert('No results found');
-              }
-            } else {
-              window.alert('Geocoder failed due to: ' + status);
-            }
-          });
+            var latlng = {lat: parseFloat(posLat), lng: parseFloat(posLng)};
+            geocoder.geocode({'location': latlng}, function (results, status) {
+                if (status === 'OK') {
+                    if (results[1]) {
+                        namePos = (results[1].formatted_address);
+                        // console.log(this);
+                        this.address = namePos;
+                    } else {
+                        window.alert('No results found');
+                    }
+                } else {
+                    window.alert('Geocoder failed due to: ' + status);
+                }
+            });
         }
+
         // Store all markers
         $scope.itemMarkers = [
-         {id:'foo', name: 'FOO SHOP', category:'Book', subCategory:'Horror', position:[59.93, 17.92], address: ''},
-         {id:'bar', name: 'BAR SHOP', category:'Game', subCategory:'Horror',  position:[59.89, 17.9], address: ''}
+            {
+                id: 'foo',
+                name: 'FOO SHOP',
+                category: 'Book',
+                subCategory: 'Horror',
+                position: [59.93, 17.92],
+                address: ''
+            },
+            {id: 'bar', name: 'BAR SHOP', category: 'Game', subCategory: 'Horror', position: [59.89, 17.9], address: ''}
         ];
 
-        $scope.showDetail = function(e, marker) {
-          $scope.marker = marker;
-          // geocodeCords(this);
-          $scope.map.showInfoWindow('mapPageIW', marker.id);
+        $scope.showDetail = function (e, marker) {
+            $scope.marker = marker;
+            // geocodeCords(this);
+            $scope.map.showInfoWindow('mapPageIW', marker.id);
         };
 
-        $http({
-            method : "GET",
-            url : "http://198.211.126.133:3000/items"
-        }).then(function mySucces(response) {
-            $scope.items = response.data;
-            // console.log($scope.items);
-        }, function myError(response) {
-            alert("Error");
-        });
+
     }]);
