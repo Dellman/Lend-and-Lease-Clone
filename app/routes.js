d337ded4bdd947e6c9776ba76270c6a34adcff27
vuxnamannen <edvin.wahlberg@gmail.com>
2017-03-11 16:43:00 +0100

    display all items and add book now works

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,11 @@ var connection = mysql.createConnection({
     database: 'lendandloan'
 });
 
+var response_object = function(code, message){
+  this.code = code;
+  this.message = message;
+}
+
 connection.connect();
 
 module.exports = function(app, passport) {
@@ -31,10 +36,14 @@ module.exports = function(app, passport) {
 	 */
     // process the login form
     app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/successlogin', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : false // allow flash messages
     }));
+
+    app.get('/successlogin', function(req, res){
+      res.send(new response_object(101, "Success at login"));
+    });
 
     // =====================================
     // SIGNUP ==============================
@@ -42,10 +51,14 @@ module.exports = function(app, passport) {
     // show the signup form
 	// process the signup form
 	app.post('/register', passport.authenticate('local-signup', {
-		successRedirect : '/', // redirect to the secure profile section
+		successRedirect : '/successregister', // redirect to the secure profile section
 		failureRedirect : '/login', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
+
+  app.get('/successregister', function(req, res){
+    res.send(new response_object(101, "Success at register"));
+  });
 
 	// =====================================
 	// PROFILE SECTION =========================
@@ -75,7 +88,7 @@ module.exports = function(app, passport) {
 	// =====================================
 	app.get('/logout', function(req, res) {
 		req.logout();
-		res.redirect('/');
+    res.send("Success on logout");
 	});
 
     // process the signup form
@@ -119,7 +132,7 @@ module.exports = function(app, passport) {
 
 app.get('/logout', function(req, res) {
         req.logout();
-        res.redirect('/');
+        res.send(new response_object(101, "Successfully logged out"));
     });
 
 app.get('/subCategories', isLoggedIn, function(req, res) {
@@ -168,6 +181,109 @@ app.get('/subCategories', isLoggedIn, function(req, res) {
   });
 });
 
+app.post('/addItem', isLoggedIn, function(req, res){
+
+  console.log("Request body: " + JSON.stringify(req.body));
+  console.log("Request user: " + JSON.stringify(req.user));
+
+  var newItem = new Object();
+  newItem.item_name = req.body.item_name;
+  newItem.description = req.body.description;
+  newItem.location = req.body.location;
+  newItem.sub_date = req.body.submission_date;
+  newItem.start_date = req.body.start_date;
+  newItem.end_date = req.body.end_date;
+  newItem.user_id = req.user;
+
+  var insertItemArray = [
+      newItem.item_name,
+      newItem.description,
+      newItem.location,
+      newItem.sub_date,
+      newItem.start_date,
+      newItem.end_date,
+      newItem.user_id
+  ]
+  var insertItemQuery = "INSERT INTO items (item_name, description, location, sub_date, start_date, end_date, user_id) values ( ? )";
+
+  console.log("InserItemArray : " + insertItemArray);
+
+    switch(req.body.category) {
+      case "Books":
+          var insertBookQuery = "INSERT INTO books (book_id, author, ISBN, date_published, book_category_id) values ( ? )";
+          var newBook = new Object();
+          newBook.author = req.body.author;
+          newBook.ISBN = req.body.ISBN;
+          newBook.date_published = req.body.date_published;
+          var category_id;
+
+          connection.query("SELECT book_category_id FROM book_categories WHERE book_category_name = '" + req.body.book_category_id + "'", function( err, rows){
+              if(!err){
+                console.log("Get category ID query rwos : " + JSON.stringify(rows));
+                console.log("GET CATEROGYR QUERY ROWS BOOK_CATEGORY ID " + rows[0].book_category_id);
+                newBook.book_category_id = rows[0].book_category_id;
+                console.log("NEW BOOK BOOK CATEGORY_ID : " + newBook.book_category_id);
+              }
+              else{
+                console.log(err);
+              }
+          });
+
+          var insert_item_id;
+          connection.query(insertItemQuery, [insertItemArray], function (err, rows) {
+              if (err) {
+                  console.log("Insertion failed");
+              }
+              else{
+                insert_item_id = rows.insertId;
+                console.log(rows.insertId);
+                console.log(JSON.stringify(rows));
+                newBook.book_id = insert_item_id;
+
+                var insertBookArray = [
+                  newBook.book_id,
+                  newBook.author,
+                  newBook.ISBN,
+                  newBook.date_published,
+                  newBook.book_category_id
+                ]
+                  console.log("Insert Book array : " + insertBookArray);
+
+                  connection.query(insertBookQuery, [insertBookArray], function (err, rows) {
+                      if (err) {
+                        console.log(err);
+                      }
+                      else{
+                        insert_item_id = rows.insertId;
+                        console.log(JSON.stringify(rows));
+                        res.send(new response_object(101, "success"));
+                      }
+                  });
+              }
+          });
+          break;
+
+      case "Orange":
+          break;
+      case "Apple":
+          break;
+      default:
+          console.log("Reached default");
+  }
+
+});
+
+app.get('/items', function(req, res){
+  connection.query("select * from items", function(err, rows){
+    if(!err)
+    {
+        res.send(rows);
+    }
+    else{
+      console.log(err);
+    }
+  });
+});
 
 // route middleware to make sure
 function isLoggedIn(req, res, next) {
@@ -177,7 +293,7 @@ function isLoggedIn(req, res, next) {
         return next();
 
     // if they aren't redirect them to the home page
-    res.redirect('/');
+    res.send(new response_object(602, "Not logged in"));
 }
 
 }
